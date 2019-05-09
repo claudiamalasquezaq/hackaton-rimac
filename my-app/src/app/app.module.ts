57bdaf25ee6c80db261aa32fb103b059b275fc22
NayRojas <nayllen.rojas@gmail.com>
2019-05-09 12:51:07 -0500

    Implementación de componentes: Navbar, Footer, Plans y estilos globales en el style.css

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PlansComponent } from './components/plans/plans.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent,
+    PlansComponent
   ],
   imports: [
     BrowserModule,
