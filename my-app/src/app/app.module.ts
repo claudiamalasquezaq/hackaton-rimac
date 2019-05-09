2f9ce454e8a819d1fbb7ebfc510f3d438db62493
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-05-09 13:03:15 -0500

    Agregando maquetado de Home y Testimonies

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { TestimoniesComponent } from './components/testimonies/testimonies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    TestimoniesComponent
   ],
   imports: [
     BrowserModule,
